3977eb4d0f709704fe8362d6a68a0a7f93810c27
JLucBlennie <jluc.blennie@gmail.com>
2016-01-12 22:03:01 +0100

    Ajout de la gestion du cache dans les requètes. Il faudra tester tout ça bientôt !!!!

--- src/com/jlb/tools/metamodel/IDataProcessorServices.java
+++ src/com/jlb/tools/metamodel/IDataProcessorServices.java
@@ -8,13 +8,13 @@ public interface IDataProcessorServices {
 
 	public void createDatabase();
 
-	public void storeObjects(List<Entity> objects);
+	public void storeEntities(List<Entity> entities);
 
 	public List<Entity> requestEntities(ICriterion criterion);
 
 	public List<Link> requestLinks(Entity entity);
 
-	public void deleteObjects(List<Entity> objects);
+	public void deleteObjects(List<Entity> entities);
 
 	public void deleteLinks(List<Link> links);
 
